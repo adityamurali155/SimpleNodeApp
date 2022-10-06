7b6c2d5e8c1e4d693474e375ea9450202c00c59f
adityamurali155 <adityamurali2@gmail.com>
2022-10-06 16:06:23 +0530

    changed message

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("Welcome to NodeJS App");
+    res.send("Welcome to NodeJS App...This app is too basic to understand");
 });
 
 app.listen(port, () => {
